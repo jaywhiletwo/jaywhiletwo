1c069045e5aeb307d59db5c7404cea5f393bb35a
Jeff Lee <jlee@fool.com>
2015-01-19 17:50:10 -0500

    basic gallery

--- photo/views.py
+++ photo/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
+from .models import Gallery
 
 
 def image(request):
-    return render(request, 'photo/show.html')
+    return render_to_response('photo/image.html', {})
 
 
-def image_set(request):
-    return render(request, 'photo/show.html')
+def image_set(request, gallery_slug=None):
+    galleries = Gallery.objects.all()
+    gallery = Gallery.objects.get(slug=gallery_slug) if gallery_slug else Gallery.objects.first()
+
+    return render_to_response('photo/image_set.html', {
+        'galleries': galleries,
+        'gallery': gallery,
+    })
