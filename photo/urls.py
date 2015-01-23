960d4595774d5615bfe5c780ed6fd5e410ff0b39
Jeff Lee <jlee@fool.com>
2015-01-23 00:30:30 -0500

    image modals

--- photo/urls.py
+++ photo/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from .views import image, image_set
+from .views import image_set
 
 
 urlpatterns = patterns('',
-    url(r'show', image, name='image', ),
     url(r'list/(?P<gallery_slug>[\w\d]+)', image_set, name='image_set', ),
     url(r'list', image_set, name='image_set', ),
 )
--- photo/views.py
+++ photo/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render_to_response
 from .models import Gallery
 
 
-def image(request):
-    return render_to_response('photo/image.html', {})
-
-
 def image_set(request, gallery_slug=None):
     galleries = Gallery.objects.all()
     gallery = Gallery.objects.get(dir_name=gallery_slug) if gallery_slug else Gallery.objects.first()
