888e92ac149b850021ec03c654164ec276c20dca
Jeff <jlee@fool.com>
2015-01-24 21:56:27 -0500

    database password update

--- project/settings/live.py
+++ project/settings/live.py
@@ -12,8 +12,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'jaywhiletwo',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'USER': 'jaywhiletwo',                      # Not used with sqlite3.
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
